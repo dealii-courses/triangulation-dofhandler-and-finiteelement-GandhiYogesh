763941c68458f0301dddc17523f6398a5be1ece4
yogesh.gandhi@unibo.it <80168409+GandhiYogesh@users.noreply.github.com>
2021-03-23 13:40:14 +0000

    step-1 complete

--- source/step-1.cc
+++ source/step-1.cc
@@ -19,6 +19,7 @@
 
 #include <deal.II/grid/grid_generator.h>
 #include <deal.II/grid/grid_out.h>
+#include <deal.II/grid/manifold_lib.h>
 #include <deal.II/grid/tria.h>
 #include <deal.II/grid/tria_accessor.h>
 #include <deal.II/grid/tria_iterator.h>
@@ -26,9 +27,17 @@
 #include <cmath>
 #include <fstream>
 #include <iostream>
+#include <tuple>
 
 using namespace dealii;
 
+std::tuple<int, int, int> func_tuple(const Triangulation<2> &tri) 
+// Function returns multivalue: # active cells, # cells, Levels
+{
+  return std::make_tuple<int, int, int>(tri.n_levels(), 
+                                        tri.n_cells(), 
+                                        tri.n_active_cells());
+}
 
 void
 first_grid()
@@ -43,6 +52,12 @@ first_grid()
   GridOut       grid_out;
   grid_out.write_svg(triangulation, out);
   std::cout << "Grid written to grid-1.svg" << std::endl;
+
+  // Triangualtion tuple construction
+  auto vals_tri = func_tuple(triangulation);
+  std::cout << "Number of Levels"<< "\t" << std::get<0>(vals_tri) << "\n" 
+            << "Number of cells"<< "\t" << std::get<1>(vals_tri) << "\n"
+            << "Number of active cells" << "\t" << std::get<2>(vals_tri) << "\n";
 }
 
 
@@ -73,6 +88,7 @@ second_grid()
                 }
             }
         }
+        triangulation.reset_manifold(0); // Reset to default manifold: Flat
 
       triangulation.execute_coarsening_and_refinement();
     }
@@ -83,29 +99,165 @@ second_grid()
   grid_out.write_svg(triangulation, out);
 
   std::cout << "Grid written to grid-2.svg" << std::endl;
+
+  // Triangualtion tuple construction
+  auto vals_tri = func_tuple(triangulation);
+  std::cout << "Number of Levels"<< "\t" << std::get<0>(vals_tri) << "\n" 
+            << "Number of cells"<< "\t" << std::get<1>(vals_tri) << "\n"
+            << "Number of active cells" << "\t" << std::get<2>(vals_tri) << "\n";
 }
 
 void
 third_grid()
 {
   Triangulation<2> triangulation;
+  const double LFT = -0.5;
+  const double RGT =  0.5;
 
-  GridGenerator::hyper_L(triangulation);
-
-  triangulation.refine_global(1);
+  GridGenerator::hyper_L(triangulation, LFT, RGT, true);
 
   std::ofstream out("grid-3.svg");
   GridOut       grid_out;
   grid_out.write_svg(triangulation, out);
   std::cout << "Grid written to grid-3.svg" << std::endl;
+    
+  // Triangualtion tuple construction
+  auto vals_tri3 = func_tuple(triangulation);
+  std::cout << "Number of Levels"<< "\t" << std::get<0>(vals_tri3) << "\n" 
+            << "Number of cells"<< "\t" << std::get<1>(vals_tri3) << "\n"
+            << "Number of active cells" << "\t" << std::get<2>(vals_tri3) << "\n";
+
+  const unsigned int initial_global_refinement = 1 ;
+
+  triangulation.refine_global(initial_global_refinement);
+  std::ofstream out_global_refine("grid-4.svg");
+
+  grid_out.write_svg(triangulation, out_global_refine);
+  std :: cout << "Grid written to grid-4.svg" << std::endl;
+
+  // Triangualtion tuple construction
+  auto vals_tri4 = func_tuple(triangulation);
+  std::cout << "Number of Levels"<< "\t" << std::get<0>(vals_tri4) << "\n" 
+            << "Number of cells"<< "\t" << std::get<1>(vals_tri4) << "\n"
+            << "Number of active cells" << "\t" << std::get<2>(vals_tri4) << "\n";
+
+  const Point<2> corner(0, 0);
+  const double corner_dist = 0.3333;
+for(unsigned int step = 0; step < 3; step++)
+{
+    for (auto &cell : triangulation.active_cell_iterators())
+    {
+      for( const auto v : cell->vertex_indices())
+      {
+        const double distance_from_corner = corner.distance(cell->center(v));
+        //std :: cout << distance_from_corner << "\n";
+
+        if(std :: fabs(distance_from_corner <= corner_dist))
+        {
+          cell->set_refine_flag();
+          break;
+        }
+        else
+        {
+          break;
+        }
+
+      }
+
+    }
+
+  triangulation.execute_coarsening_and_refinement();
+}
+  std :: ofstream out_reentrant("grid-5.svg");
+
+  grid_out.write_svg(triangulation, out_reentrant);
+  std :: cout << "Grid written to grid-5.svg" <<std :: endl;
+
+  // Triangualtion tuple construction
+  auto vals_tri5 = func_tuple(triangulation);
+  std::cout << "Number of Levels"<< "\t" << std::get<0>(vals_tri5) << "\n" 
+            << "Number of cells"<< "\t" << std::get<1>(vals_tri5) << "\n"
+            << "Number of active cells" << "\t" << std::get<2>(vals_tri5) << "\n";
+}
+
+void
+spherical_grid()
+{
+  const Point<2> center(0, 0);
+  const double outer_radius = 1.0;
+
+  Triangulation<2> triangulation;
+  GridGenerator :: hyper_ball(triangulation, center, outer_radius);
+  triangulation.reset_all_manifolds();
+  triangulation.set_all_manifold_ids_on_boundary(0);
+
+  std::ofstream out("grid-6.svg");
+  GridOut       grid_out;
+  grid_out.write_svg(triangulation, out);
+  std::cout << "Grid written to grid-6.svg" << std::endl;
+
+  // Spherical Manifold on Boundary
+
+  const SphericalManifold<2> manifold(center);
+  Triangulation<2> tria_boundary;
+
+  GridGenerator :: hyper_ball(tria_boundary, center, outer_radius, true);
+  tria_boundary.reset_all_manifolds();
+  tria_boundary.set_all_manifold_ids_on_boundary(0);
+  tria_boundary.set_manifold(0, manifold);
+  tria_boundary.refine_global(2);
+
+  std::ofstream out_boundary("grid-7.svg");
+  grid_out.write_svg(tria_boundary, out_boundary);
+  std::cout << "Grid written to grid-7.svg" << std::endl;
+
+  // Spherical Manifold Everywhere
+
+  Triangulation<2> tria_everywhere;
+
+  GridGenerator :: hyper_ball(tria_everywhere, center, outer_radius, true);
+  tria_everywhere.reset_all_manifolds();
+  tria_everywhere.set_all_manifold_ids(0);
+  tria_everywhere.set_manifold(0, manifold);
+  tria_everywhere.refine_global(2);
+
+  std::ofstream out_eveywhere("grid-8.svg");
+  grid_out.write_svg(tria_everywhere, out_eveywhere);
+  std::cout << "Grid written to grid-8.svg" << std::endl;
+
+  // Spherical Manifold Except center
+
+  Triangulation<2> tria_except_center;
+
+  GridGenerator :: hyper_ball(tria_except_center, center, outer_radius, true);
+  tria_except_center.reset_all_manifolds();
+  for (unsigned int step = 0; step <2; step++)
+  {
+    for (auto &cell : tria_except_center.active_cell_iterators())
+    {
+      if (std :: fabs(cell->center().norm() > 0))
+      {
+        tria_except_center.set_all_manifold_ids(0);
+        tria_except_center.set_manifold(0, manifold);
+        cell->refine_flag_set();
+      }
+      tria_except_center.refine_global();
+    }
+    tria_except_center.execute_coarsening_and_refinement();
+  }
+  std::ofstream out_except_center("grid-9.svg");
+  grid_out.write_svg(tria_except_center, out_except_center);
+  std::cout << "Grid written to grid-9.svg" << std::endl;
+
+  
 }
 
 
-
-int
-main()
+int main()
 {
   first_grid();
   second_grid();
   third_grid();
+  spherical_grid();
 }
+
